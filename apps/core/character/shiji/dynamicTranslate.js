bc3c1e10d1eac46726d56b337486666afee38cd6
vtpham23 <phamthanhvinh90@gmail.com>
2026-04-21 17:34:59 +0700

    trans shiji dynamic

--- apps/core/character/shiji/dynamicTranslate.js
+++ apps/core/character/shiji/dynamicTranslate.js
@@ -3,7 +3,7 @@ import { lib, game, ui, get, ai, _status } from "noname";
 const dynamicTranslates = {
 	spzhenting(player, skill) {
 		if (player.hasSkill("spjincui_delete")) {
-			return "每回合限一次。当你或你攻击范围内的角色成为【杀】或延时锦囊的目标时，若你不是此牌的使用者，你可选择一项：①弃置使用者的一张手牌；②摸一张牌。"
+			return "Mỗi vòng giới hạn một lần. Khi bạn hoặc một nhân vật trong phạm vi tấn công của bạn trở thành mục tiêu của【Sát】hoặc một tây dược trì hoãn, nếu bạn không phải là người sử dụng tây này, bạn có thể chọn một trong những điều sau: ① Loại bỏ một lá bài ở tay của người sử dụng; ② Rút một lá bài."
 		}
 		return lib.translate[`${skill}_info`];
 	},
