4d027642b9f92e1efe5cd9ec558117c558b9a6bb
vtpham23 <phamthanhvinh90@gmail.com>
2026-04-21 10:17:43 +0700

    translate prompt ky nang sp2 bingshi

--- apps/core/character/bingshi/dynamicTranslate.js
+++ apps/core/character/bingshi/dynamicTranslate.js
@@ -1,47 +1,45 @@
 import { lib, game, ui, get, ai, _status } from "noname";
 
 const dynamicTranslates = {
-	mbkechang(player, skill) {
-		if (player.getStorage(skill, false)) {
-			return lib.translate[`${skill}_rewrite_info`];
-		}
-		return lib.translate[`${skill}_info`];
-	},
-	potkuanggu(player) {
-		if (player.getStorage("potkuanggu", 0)) {
-			return lib.translate["potkuanggu_pot_weiyan_achieve_info"];
-		}
-		return lib.translate["potkuanggu_info"];
-	},
-	pothanzhan(player) {
-		let str = lib.translate.pothanzhan_info;
-		if (!player.storage.pothanzhan) {
-			return str;
-		}
-		return str.replace(
-			"X为各自体力上限",
-			"X为" +
-				{
-					hp: "各自体力值",
-					damagedHp: "各自损失体力值",
-					countplayer: "场上存活角色数",
-				}[player.storage.pothanzhan]
-		);
-	},
-	potzhanlie(player) {
-		let str = lib.translate.potzhanlie_info;
-		if (!player.storage.potzhanlie) {
-			return str;
-		}
-		return str.replace(
-			"X为你的攻击范围",
-			"X为" +
-				{
-					hp: "你的体力值",
-					damagedHp: "你的损失体力值",
-					countplayer: "场上存活角色数",
-				}[player.storage.potzhanlie]
-		);
-	},
+  mbkechang(player, skill) {
+    if (player.getStorage(skill, false)) {
+      return lib.translate[`${skill}_rewrite_info`];
+    }
+    return lib.translate[`${skill}_info`];
+  },
+  potkuanggu(player) {
+    if (player.getStorage("potkuanggu", 0)) {
+      return lib.translate["potkuanggu_pot_weiyan_achieve_info"];
+    }
+    return lib.translate["potkuanggu_info"];
+  },
+  pothanzhan(player) {
+    let str = lib.translate.pothanzhan_info;
+    if (!player.storage.pothanzhan) {
+      return str;
+    }
+    return str.replace(
+      "X là giới hạn thể lực mỗi bên",
+      "X là " + {
+        hp: "trị số thể lực mỗi bên",
+        damagedHp: "trị số thể lực đã mất mỗi bên",
+        countplayer: "số lượng nhân vật còn sống trên sân"
+      }[player.storage.pothanzhan]
+    );
+  },
+  potzhanlie(player) {
+    let str = lib.translate.potzhanlie_info;
+    if (!player.storage.potzhanlie) {
+      return str;
+    }
+    return str.replace(
+      "X là tầm tấn công của bạn",
+      "X là " + {
+        hp: "trị số thể lực của bạn",
+        damagedHp: "trị số thể lực đã mất của bạn",
+        countplayer: "số lượng nhân vật còn sống trên sân"
+      }[player.storage.potzhanlie]
+    );
+  }
 };
 export default dynamicTranslates;
--- apps/core/character/sp2/dynamicTranslate.js
+++ apps/core/character/sp2/dynamicTranslate.js
@@ -9,44 +9,44 @@ const dynamicTranslates = {
 	},
 	mubing(player) {
 		if (player.storage.mubing2) {
-			return "出牌阶段开始时，你可以亮出牌堆顶的四张牌。你可弃置任意张手牌，并可获得任意张点数之和不大于你弃置的牌点数之和的牌。然后你可将以此法得到的牌以任意方式交给其他角色。";
+			return "Khi bắt đầu giai đoạn ra bài, bạn có thể lật sáng bốn lá bài từ trên cùng xấp bài. Bạn có thể vứt bỏ số lá bài bất kỳ trên tay, và có thể nhận số lá bài bất kỳ có tổng điểm không lớn hơn tổng điểm của các lá bài bạn đã vứt bỏ. Sau đó bạn có thể đem số bài nhận được theo cách này đưa cho các nhân vật khác theo cách bất kỳ.";
 		}
-		return "出牌阶段开始时，你可以亮出牌堆顶的三张牌。你可弃置任意张手牌，并可获得任意张点数之和不大于你弃置的牌点数之和的牌。";
+		return "Khi bắt đầu giai đoạn ra bài, bạn có thể lật sáng ba lá bài từ trên cùng xấp bài. Bạn có thể vứt bỏ số lá bài bất kỳ trên tay, và có thể nhận số lá bài bất kỳ có tổng điểm không lớn hơn tổng điểm của các lá bài bạn đã vứt bỏ.";
 	},
 	piaoping(player) {
 		const bool = player.storage.piaoping;
-		let yang = "你摸X张牌",
-			yin = "你弃置X张牌";
+		let yang = "bạn rút X lá bài",
+			yin = "bạn vứt bỏ X lá bài";
 		if (bool) {
 			yin = `<span class='bluetext'>${yin}</span>`;
 		} else {
 			yang = `<span class='firetext'>${yang}</span>`;
 		}
-		let start = "转换技，锁定技。当你使用一张牌时，",
-			end = "（X为你本阶段内发动过〖漂萍〗的次数且至多等于你的体力值）。";
-		return `${start}阳：${yang}；阴：${yin}${end}`;
+		let start = "Chuyển hoán kỹ, Tỏa định kỹ. Khi bạn sử dụng một lá bài, ",
+			end = " (X là số lần bạn đã phát động 〖Phiêu Bình〗 trong giai đoạn này và tối đa bằng trị số thể lực của bạn).";
+		return `${start}Dương: ${yang}; Âm: ${yin}${end}`;
 	},
 	zhuili(player) {
 		if (!player.hasSkill("piaoping", null, null, false)) {
-			return "锁定技。当你成为其他角色使用黑色牌的目标后，若你的〖漂萍〗：处于阳状态，则你将〖漂萍〗转换至阴状态；处于阴状态，则你令〖托献〗发动次数+1，然后若〖托献〗发动次数大于3，则〖惴栗〗于本回合内失效。";
+			return "Tỏa định kỹ. Khi bạn trở thành mục tiêu của lá bài màu đen do nhân vật khác sử dụng, nếu 〖Phiêu Bình〗 của bạn: đang ở trạng thái Dương, thì bạn chuyển 〖Phiêu Bình〗 sang trạng thái Âm; đang ở trạng thái Âm, thì bạn khiến số lần phát động 〖Thác Hiến〗 +1, sau đó nếu số lần phát động 〖Thác Hiến〗 lớn hơn 3, thì 〖Chủy Lật〗 vô hiệu trong hiệp này.";
 		}
 		if (player.storage.piaoping) {
-			return '锁定技。当你成为其他角色使用黑色牌的目标后，若你的〖漂萍〗：<span class="bluetext">处于阳状态，则你将〖漂萍〗转换至阴状态；</span>处于阴状态，则你令〖托献〗发动次数+1，然后若〖托献〗发动次数大于3，则〖惴栗〗于本回合内失效。';
+			return 'Tỏa định kỹ. Khi bạn trở thành mục tiêu của lá bài màu đen do nhân vật khác sử dụng, nếu 〖Phiêu Bình〗 của bạn: <span class="bluetext">đang ở trạng thái Dương, thì bạn chuyển 〖Phiêu Bình〗 sang trạng thái Âm;</span> đang ở trạng thái Âm, thì bạn khiến số lần phát động 〖Thác Hiến〗 +1, sau đó nếu số lần phát động 〖Thác Hiến〗 lớn hơn 3, thì 〖Chủy Lật〗 vô hiệu trong hiệp này.';
 		}
-		return '锁定技。当你成为其他角色使用黑色牌的目标后，若你的〖漂萍〗：处于阳状态，则你将〖漂萍〗转换至阴状态；<span class="bluetext">处于阴状态，则你令〖托献〗发动次数+1，然后若〖托献〗发动次数大于3，则〖惴栗〗于本回合内失效。</span>';
+		return 'Tỏa định kỹ. Khi bạn trở thành mục tiêu của lá bài màu đen do nhân vật khác sử dụng, nếu 〖Phiêu Bình〗 của bạn: đang ở trạng thái Dương, thì bạn chuyển 〖Phiêu Bình〗 sang trạng thái Âm; <span class="bluetext">đang ở trạng thái Âm, thì bạn khiến số lần phát động 〖Thác Hiến〗 +1, sau đó nếu số lần phát động 〖Thác Hiến〗 lớn hơn 3, thì 〖Chủy Lật〗 vô hiệu trong hiệp này.</span>';
 	},
 	dcdouzhen(player) {
 		const bool = player.countMark("dcdouzhen") % 2;
-		let yang = "黑色基本牌均视为【决斗】，你的回合内：当你使用非转化且对应的实体牌为一张黑色基本牌的【决斗】时，你获得目标角色各一张牌并获得1枚“☯”",
-			yin = "红色基本牌均视为无次数限制的普【杀】，你的回合内：当你使用或打出非转化且对应的实体牌为一张红色基本牌的【杀】时，你获得1枚“☯”";
+		let yang = "các lá bài Cơ bản màu đen đều xem là 【Quyết Đấu】, trong hiệp của bạn: khi bạn sử dụng 【Quyết Đấu】 không phải dạng chuyển hóa và có lá bài thực thể tương ứng là một lá bài Cơ bản màu đen, bạn nhận của mỗi nhân vật mục tiêu một lá bài và nhận 1 dấu ấn “☯”",
+			yin = "các lá bài Cơ bản màu đỏ đều xem là 【Sát】 thường không giới hạn số lần sử dụng, trong hiệp của bạn: khi bạn sử dụng hoặc đánh ra 【Sát】 không phải dạng chuyển hóa và có lá bài thực thể tương ứng là một lá bài Cơ bản màu đỏ, bạn nhận 1 dấu ấn “☯”";
 		if (bool) {
 			yin = `<span class='bluetext'>${yin}</span>`;
 		} else {
 			yang = `<span class='firetext'>${yang}</span>`;
 		}
-		let start = "锁定技。转换技。你的",
-			end = "。";
-		return `${start}阳：${yang}；阴：${yin}${end}`;
+		let start = "Tỏa định kỹ. Chuyển hoán kỹ. Các lá bài ",
+			end = " của bạn.";
+		return `${start}Dương: ${yang}; Âm: ${yin}${end}`;
 	},
 };
 
