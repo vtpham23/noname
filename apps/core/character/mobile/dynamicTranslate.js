4bf4f9bef04db810906789441074e7d7e6a3f9a3
vtpham23 <phamthanhvinh90@gmail.com>
2026-04-22 11:42:33 +0700

    dyn translate mobile

--- apps/core/character/mobile/dynamicTranslate.js
+++ apps/core/character/mobile/dynamicTranslate.js
@@ -23,39 +23,39 @@ const dynamicTranslates = {
 				return get.skillInfoTranslation(trueSkill, player, false);
 			}
 		}
-		return "这衣服，岂是你配穿的？";
+		return "Bộ quần áo này, há phải ngươi xứng mặc?";
 	},
 	mbfozong(player) {
 		const list = player.getStorage("mbfozong");
 		if (!list?.length) {
 			return lib.translate["mbfozong"];
 		}
-		const colors = list?.map(i => get.translation(i))?.join("和");
-		return `锁定技，你的${colors}手牌不计入手牌上限，造成的伤害值和恢复值+1。`;
+		const colors = list?.map(i => get.translation(i))?.join(" và ");
+		return `Khóa định kỹ, bài trên tay màu ${colors} của ngươi không tính vào giới hạn bài trên tay, giá trị sát thương gây ra và giá trị hồi phục +1。`;
 	},
 	yizan_use(player) {
 		if (player.storage.yizan) {
-			return "你可以将一张基本牌当做任意基本牌使用或打出。";
+			return "Ngươi có thể dùng một lá bài cơ bản như bất kỳ lá bài cơ bản nào để sử dụng hoặc đánh ra。";
 		}
-		return "你可以将两张牌（其中至少一张为基本牌）当做任意基本牌使用或打出。";
+		return "Ngươi có thể dùng hai lá bài (trong đó ít nhất một lá là bài cơ bản) như bất kỳ sử dụng hoặc đả xuất";
 	},
 	miaojian(player) {
-		return ["出牌阶段限一次。你可将一张【杀】当做刺【杀】使用，或将一张锦囊牌当做【无中生有】使用。", "出牌阶段限一次。你可将一张基本牌当做刺【杀】使用，或将一张非基本牌当做【无中生有】使用。", "出牌阶段限一次。你可视为使用一张刺【杀】或【无中生有】。"][player.countMark("miaojian")];
+		return ["Giới hạn một lần mỗi giai đoạn ra bài. Bạn có thể dùng một lá【Sát】như【Thích Sát】, hoặc dùng một lá bài cẩm nang như【Vô trung sinh hữu】.", "Giới hạn một lần mỗi giai đoạn ra bài. Bạn có thể dùng một lá bài cơ bản như【Thích Sát】, hoặc dùng một lá bài phi cơ bản như【Vô Trung Sinh Hữu】.", "Giới hạn một lần mỗi giai đoạn ra bài. Bạn có thể coi như đã sử dụng một lá【Thích Sát】 hoặc【Vô Trung Sinh Hữu】."][player.countMark("miaojian")];
 	},
 	shhlianhua(player) {
-		return ["当你成为【杀】的目标后，你摸一张牌。", "当你成为【杀】的目标后，你摸一张牌。然后你进行判定，若结果为黑桃，则此【杀】对你无效。", "当你成为【杀】的目标后，你摸一张牌。然后此【杀】的使用者选择一项：①弃置一张牌。②令此【杀】对你无效。"][player.countMark("shhlianhua")];
+		return ["Khi bạn trở thành mục tiêu của【Sát】, bạn rút một lá bài.", "Khi bạn trở thành mục tiêu của【Sát】, bạn rút một lá bài. Sau đó bạn phán định, nếu kết quả là bích, thì【Sát】này vô hiệu với bạn.", "Khi bạn trở thành mục tiêu của【Sát】, bạn rút một lá bài. Sau đó người dùng【Sát】này chọn một: ①Bỏ một lá bài. ②Khiến【Sát】này vô hiệu với bạn."][player.countMark("shhlianhua")];
 	},
 	mobilexingxue(player) {
 		return lib.translate[(player.storage.mobileyanzhu ? "mobilexingxuex" : "mobilexingxue") + "_info"];
 	},
 	shoufa(player) {
 		const zhoufa = player.storage.zhoulin_zhoufa;
-		const nodoudizhu = get.mode() == "doudizhu" ? "与你距离不小于1的/距离不大于1的" : "与你距离不小于2的/距离不大于2的";
+		const nodoudizhu = get.mode() == "doudizhu" ? "nhân vật có khoảng cách không nhỏ hơn 1/không lớn hơn 1 với bạn" : "nhân vật có khoảng cách không nhỏ hơn 2/không lớn hơn 2 với bạn";
 		if (!zhoufa) {
-			return "当你受到伤害后/于一回合首次造成伤害后，你可以选择一名" + nodoudizhu + "角色，令其随机执行以下一项：豹，令其受到1点无来源伤害；鹰，你随机获得其一张牌；熊，你随机弃置其装备区的一张牌；兔，令其摸一张牌。";
+			return "Khi bạn nhận sát thương/lần đầu gây sát thương trong một lượt, bạn có thể chọn một " + nodoudizhu + ", khiến họ thực hiện ngẫu nhiên một trong các mục sau: Báo, khiến họ nhận 1 điểm sát thương không có nguồn; Ưng, bạn lấy ngẫu nhiên một lá bài của họ; Gấu, bạn bỏ ngẫu nhiên một lá bài trong vùng trang bị của họ; Thỏ, khiến họ rút một lá bài.";
 		}
-		let str = "当你受到伤害后/于一回合首次造成伤害后，你可以选择一名" + nodoudizhu + "角色，";
-		str += ["令其受到1点无来源伤害", "你随机获得其一张牌", "你随机弃置其装备区的一张牌", "令其摸一张牌"][["豹", "鹰", "熊", "兔"].indexOf(zhoufa)];
+		let str = "Khi bạn nhận sát thương/lần đầu gây sát thương trong một lượt, bạn có thể chọn một " + nodoudizhu + ", ";
+		str += ["khiến họ nhận 1 điểm sát thương không có nguồn", "bạn lấy ngẫu nhiên một lá bài của họ", "bạn bỏ ngẫu nhiên một lá bài trong vùng trang bị của họ", "khiến họ rút một lá bài"][["豹", "鹰", "熊", "兔"].indexOf(zhoufa)];
 		return str + "。";
 	},
 	mbxuetu(player) {
@@ -63,45 +63,48 @@ const dynamicTranslates = {
 			status = player.countMark("mbxuetu_status");
 		if (status === 1) {
 			return lib.translate.mbxuetu_achieve_info;
+
+
 		}
-		let yang = status === 0 ? "你可以令一名角色回复1点体力" : "你可以回复1点体力，然后令一名其他角色弃置两张牌",
-			yin = status === 0 ? "你可以令一名角色摸两张牌" : "你可以摸一张牌，然后对一名其他角色造成1点伤害";
+		let yang = status === 0 ? "Bạn có thể khiến một nhân vật hồi phục 1 điểm thể lực" : "Bạn có thể hồi phục 1 điểm thể lực, sau đó khiến một nhân vật khác bỏ hai lá bài",
+			yin = status === 0 ? "Bạn có thể khiến một nhân vật rút hai lá bài" : "Bạn có thể rút một lá bài, sau đó gây 1 điểm sát thương cho một nhân vật khác";
 		if (bool) {
 			yin = `<span class='bluetext'>${yin}</span>`;
 		} else {
 			yang = `<span class='firetext'>${yang}</span>`;
 		}
-		let start = "转换技。出牌阶段限一次，",
+		let start = "Kỹ năng chuyển đổi. Giới hạn một lần mỗi giai đoạn ra bài, ",
 			end = "。";
-		return `${start}阳：${yang}；阴：${yin}${end}`;
+		return `${start}Dương：${yang}；Âm：${yin}${end}`;
 	},
 	mbzuoyou(player) {
 		const bool = player.storage.mbzuoyou;
-		let yang = "你可以令一名角色摸三张牌，然后其弃置两张牌",
-			yin = "你可以令一名角色获得1点护甲";
+		let yang = "Bạn có thể khiến một nhân vật rút ba lá bài, sau đó nhân vật đó bỏ hai lá bài",
+			yin = "Bạn có thể khiến một nhân vật nhận 1 điểm giáp";
 		if (bool) {
 			yin = `<span class='bluetext'>${yin}</span>`;
 		} else {
 			yang = `<span class='firetext'>${yang}</span>`;
 		}
-		let start = "转换技。出牌阶段限一次，",
+		let start = "Kỹ năng chuyển đổi. Giới hạn một lần mỗi giai đoạn ra bài, ",
 			end = "。";
-		return `${start}阳：${yang}；阴：${yin}${end}`;
+		return `${start}Dương：${yang}；Âm：${yin}${end}`;
 	},
 	mbfunan(player) {
 		if (player.storage.mbfunan_rewrite) {
-			return `其他角色使用或打出牌响应你使用的牌时，你可以获得其使用或打出的牌，且你使用以此法获得的牌结算结束后，若没有其他角色响应此牌，你摸一张牌。`;
+			return `Khi các nhân vật khác sử dụng hoặc đánh bài để phản hồi bài bạn sử dụng, bạn có thể lấy bài mà họ đã sử dụng hoặc đánh ra, và sau khi kết thúc tính toán bài bạn sử dụng bằng cách này, nếu không có nhân vật khác phản hồi bài đó, bạn rút một lá bài.`;
 		}
-		return `其他角色使用或打出牌响应你使用的牌时，你可以获得其使用或打出的牌。`;
+		return `Khi các nhân vật khác sử dụng hoặc đánh bài để phản hồi bài bạn sử dụng, bạn có thể lấy bài mà họ đã sử dụng hoặc đánh ra.`;
 	},
 	mbxuehen(player) {
 		const storage = player.storage.mbxuehen;
-		let str = "当你每回合首次造成或受到伤害后，你可展示至多X张手牌（X为你已损失体力值），这些牌只能当不计入次数且无次数限制的【杀】使用或打出，直到你使用这些牌造成伤害";
+		let str = "Sau lần đầu tiên trong mỗi lượt bạn gây ra hoặc nhận sát thương, bạn có thể trưng ra tối đa X lá bài trên tay (X là số điểm thể lực đã mất), những lá bài này chỉ có thể được sử dụng hoặc đánh ra như【Sát】không tính vào số lần và không giới hạn số lần sử dụng, cho đến khi bạn gây sát thương bằng những lá bài này";
 		if (storage) {
-			str += "，且你使用以此法转化的【杀】结算结束后摸一张牌";
+			str += "，và sau khi kết thúc tính toán【Sát】được chuyển đổi bằng cách này, bạn rút một lá bài";
 		}
 		str += "。";
 		return str;
 	},
 };
 export default dynamicTranslates;
+
